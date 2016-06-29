3a36c5e8f16a4a891ba5078b2aef6694861c5694
Godsten Silver <geeausten@gmail.com>
2016-06-29 08:20:36 -0700

    fixed coded triangle numbers

--- nodeFileIO.js
+++ nodeFileIO.js
@@ -1,5 +1,4 @@
 var fs = require('fs');
-console.log("banana");
 
 function getRecordsFromFile(filename, cb) {
 
@@ -20,20 +19,20 @@ var codedTriangleNumbers = function(wordsArr){
   var testString;
   var currentSum = 0;
   var count = 0;
-  console.log(wordsArr[0]);
+
   for (var i = 0; i < wordsArr.length; i++) {
     currentSum = 0;
 
     for (var j = 0; j < wordsArr[i].length; j++) {
-      currentSum += wordsArr[i][j].charCodeAt(0) - "A".charCodeAt(0) + 1;
-    }
-    
-    if(isTriangleNum(currentSum)){
-      if(currentSum === 1){
-        console.log(wordsArr[i]);
+      if(wordsArr[i][j] !== '"'){
+        currentSum += wordsArr[i][j].charCodeAt(0) - "A".charCodeAt(0) + 1;
+
       }
-      // console.log(wordsArr[i]);
-      count +=1;
+    }
+
+    if(isTriangleNum(currentSum)){
+      console.log(wordsArr[i]);
+      count += 1;
     }
 
   }
@@ -56,51 +55,10 @@ var isTriangleNum = function(num){
   }
   return false;
 };
-var isPrime = function(num){
-  if (num === 2){
-    return true;
-  }
-  if (num < 2 || num % 2 === 0){
-    return false;
-  }
-  var i = 3;
-  while(i < num){
-    if(num % i === 0){
-      return false;
-    }
-    i += 2;
-  }
-  return true;
-};
 
-var getFactors = function(number){
-  if(number <1){
-    return "not yet implemented";
-  }
-  if(number === 1){
-    return [1];
-  }
-  var factors = [1];
-  if(isPrime(number)){
-    factors.push(number);
-    return factors;
-  }
-  var i = 2;
-  var theSqrt = Math.sqrt(number);
-  while(i < theSqrt){
-    if(number % i === 0){
-      factors.push(i);
-      factors.push(number/i);
-    }
-    i++;
-  }
-  if(theSqrt === Math.floor(theSqrt)){
-    factors.push(theSqrt);
-    factors.push(theSqrt);
-  }
-  return factors;
-};
+
 
 // codedTriangleNumbers(['A']);
 
+console.log("A".charCodeAt(0));
 var records = getRecordsFromFile("p042_words.txt", codedTriangleNumbers);
